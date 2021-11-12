eeb70a0d7e182f5c29c8d6c880ee2d47132cdc3b
chiarabales <69734963+chiarabales@users.noreply.github.com>
2021-11-12 16:45:31 +0100

    Add files via upload

--- orderings_computation.py
+++ orderings_computation.py
@@ -20,12 +20,10 @@ def main(geneset, ordering):
     _data_input = os.path.join('..', 'data/' + geneset)
     _plot_path = os.path.join('..', 'plot_genesets')
     path = os.path.join(_data_input, geneset + '.csv')
-
     if geneset == 'KEGG':
         gen = pd.read_csv(path, sep = ';', header = None)
     else:
         gen = pd.read_csv(path, sep = ',', header = None)
-
     gen = gen.drop(columns = 1)
     data = dp.transform_gen(gen, geneset)
     max_ranking = np.shape(data)[0]
@@ -35,29 +33,26 @@ def main(geneset, ordering):
         mydata = mydata.drop(columns = 'set')
         mydata = np.asarray(mydata)
         SV_pathways = sg.calculate_svs(mydata)
-        
         data_new = data.copy()
-        data_new['SV'] = SV_pathways
-        data_new = data_new.sort_values(by = 'SV', ascending=False)
-        
+        data_new['SV_original'] = SV_pathways
+        data_new = data_new.sort_values(by = 'SV_original', ascending=False)
         order = []
-        for row in result_for_pathways[['set', 'SV']].index:
+        for row in data_new[['set', 'SV_original']].index:
             order.append(row)
-        
+
     elif ordering == 'PO':
         order = g.PO(data, 'N', max_ranking = max_ranking)
-        
+
     elif ordering == 'POR':
         order = g.PO(data, 'R', max_ranking = max_ranking)
-        
+
     elif ordering == 'AO':
         order = g.AO(data, 'N', max_ranking = max_ranking)
-        
+
     elif ordering == 'AOR':
         order = g.AO(data,  'R', max_ranking = max_ranking)
-    
+            
     return order
-
 
 if __name__ == '__main__':
     main()
